9a4735e28006224892d9e4c5594eaa374cbcaf96
mhlehri <lehrimirza101@gmail.com>
2023-12-21 02:23:24 +0600

    navbar created

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,26 +1,41 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Home = () => {
+  const [open, setOpen] = useState(false);
   return (
     <div>
       <header>
-        <nav className="flex items-center mx-auto max-w-screen-xl p-5 bg-red-300">
+        <nav className="flex items-center mx-auto max-w-screen-xl p-5">
           <div className="logo absolute">
             {/* <img src="" alt="" /> */}
             <h1 className="text-4xl">logo</h1>
           </div>
           <div className="flex justify-center mx-auto text-center">
-            <ul className="hidden font-semibold lg:flex gap-5 items-center">
+            <ul className="hidden font-medium lg:flex gap-5 items-center">
               <Link to="/">Home</Link>
               <Link to="/about">About</Link>
               <Link to="contact">Contact</Link>
               <Link to="pricing">Pricing</Link>
             </ul>
           </div>
-          {/* <ul>
-            <li></li>
-          </ul> */}
+          <img
+            onClick={() => {
+              setOpen(!open);
+            }}
+            className="block lg:hidden"
+            src="/public/menu.svg"
+            width={30}
+            alt="menu"
+          />
         </nav>
+
+        <ul className="lg:hidden font-medium flex-col flex gap-5 items-center">
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="contact">Contact</Link>
+          <Link to="pricing">Pricing</Link>
+        </ul>
       </header>
     </div>
   );
